8a3bdef3c4eff9cf55646741e263faa9a79b815c
Jonatan <this.pablo@gmail.com>
2022-02-22 15:45:25 -0300

    feat: fixed timeConversion
     Fixed conversion for 12h and 00h
     Added padStart for 01h to 09h AM

--- hackerrank/time-conversion.js
+++ hackerrank/time-conversion.js
@@ -1,18 +1,34 @@
-function timeConversion(s) {
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+const getFinalHour = (hour, complement) => {
+    if (hour % 12 === 0){
+        return Math.abs(hour - (12 + complement));
+    }
+    
+    const newHour = hour + complement;
+    return newHour;
+}
+
+function timeConversion(s) {   
     const getComplement = { 
         'AM' : 0,
         'PM' : 12,
     };
-    
-    const dateLength = s.length;
-    const partialDate = s.substring(2, dateLength-2);
-    
-    const hourFormatMatches = s.match(/\w{2}$/);
-    const complement = getComplement[hourFormatMatches[0]];
 
     const hourMatches = s.match(/^\d{2}/);
+    const hourFormatMatches = s.match(/\w{2}$/);
+
     const hour = parseInt(hourMatches[0]);
-    
-    return `${hour + complement}` + partialDate
+    const complement = getComplement[hourFormatMatches[0]];
+
+    const finalHour = getFinalHour(hour, complement);
+    const partialDate = s.substring(2, s.length-2);
+
+    return `${finalHour}`.padStart(2, 0) + partialDate;
 
 }
